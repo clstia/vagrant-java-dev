c78f07f865a953bbc21ae44e4bf55072b481b11a
cternes <github@slackspace.de>
2014-08-03 12:13:17 +0200

    Added proxy for apt

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+#use an apt-cacher as proxy
+echo "echo 'Acquire::http { Proxy "http://apt-cacher-ip:3142"; };' > /etc/apt/apt.conf.d/01proxy"
 
 #map glassfish-autodeploy folder to autodeploy in glassfish directory
 rm -rf /opt/glassfish/glassfish4/glassfish/domains/domain1/autodeploy
